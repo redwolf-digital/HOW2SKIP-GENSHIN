556215e624ec274de1084f4fc9f9ff5a3794b515
redwolf-digital <worakan9012@gmail.com>
2022-08-27 12:11:45 +0700

    UPDATE Ver.0.1.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
-#include <Arduino.h>
-#include "Keyboard.h"
+//TEST
 
-void setup() {
-  
+#include <Arduino.h>
+#include <Keyboard.h>
+
+bool Butt_Flags = 0;
+
+
+void Butt_Event() {
+  if(digitalRead(PB10) == LOW) {
+    Butt_Flags = !Butt_Flags;
+    delay(150);
+  }
 }
 
+
+void setup() {
+  Keyboard.begin();
+  pinMode(PC13, OUTPUT);
+  pinMode(PB10, INPUT_PULLUP);
+  digitalWrite(PC13, HIGH);
+
+  attachInterrupt(digitalPinToInterrupt(PB10), Butt_Event, FALLING);
+}
+
+
+//TEST
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Butt_Flags == 1) {
+    digitalWrite(PC13, LOW);
+    delay(100);
+    digitalWrite(PC13, HIGH);
+    delay(100);
+
+  }else {
+    digitalWrite(PC13, LOW);
+    delay(1000);
+    digitalWrite(PC13, HIGH);
+    delay(1000);
+  }
 }
