c0f09ad4202dd1be236f5c62e602276101a8bc9c
redwolf-digital <worakan9012@gmail.com>
2022-08-27 20:57:31 +0700

    UPDATE Ver.1.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@ bool Butt_Flags = 0;
 void Butt_Event() {
   if(digitalRead(PB10) == LOW) {
     Butt_Flags = !Butt_Flags;
+    
+    if(Butt_Flags == 1) {
+      digitalWrite(PC13, LOW);
+    }else{
+      digitalWrite(PC13, HIGH);
+      Mouse.release(MOUSE_LEFT);
+    }
     delay(150);
   }
 }
@@ -14,21 +21,20 @@ void Butt_Event() {
 
 void setup() {
   Mouse.begin();
+
   pinMode(PC13, OUTPUT);
   pinMode(PB10, INPUT_PULLUP);
   digitalWrite(PC13, HIGH);
-
   attachInterrupt(digitalPinToInterrupt(PB10), Butt_Event, FALLING);
 }
 
 
-//TEST
 void loop() {
   if(Butt_Flags == 1) {
-    digitalWrite(PC13, LOW);
-    Mouse.click();
-    delay(250);
-  }else {
-    digitalWrite(PC13, HIGH);
+    Mouse.click(MOUSE_LEFT);
+  }
+  //Assembly "NOP" to stop CPU operation for Delay  
+  for(unsigned int i=0; i<250; i++) {
+    asm volatile("nop");
   }
 }
